154510ac515a9b960fef3f0cc52acb546545519a
chenshuai2144 <qixian.cs@outlook.com>
2019-11-23 23:51:18 +0800

    add skipLibCheck

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,22 @@
-import * as ts from "typescript";
-import compile from "./compile";
-import path, { join } from "path";
-import importFresh from "import-fresh";
+import * as ts from 'typescript';
+import compile from './compile';
+import path, { join } from 'path';
+import importFresh from 'import-fresh';
 
 export = (filePath: string) => {
-  const outDir = join(__dirname, "../.cache");
-  const fileName = path.basename(filePath).replace(".ts", ".js");
+  const outDir = join(__dirname, '../.cache');
+  const fileName = path.basename(filePath).replace('.ts', '.js');
   const index = compile([filePath], {
     noEmitOnError: true,
     noImplicitAny: false,
     allowJs: true,
     target: ts.ScriptTarget.ES5,
     module: ts.ModuleKind.CommonJS,
-    outDir
+    outDir,
+    skipLibCheck: true,
   } as ts.CompilerOptions);
   if (index !== 0) {
-    throw new Error("编译失败");
+    throw new Error('编译失败');
   }
   const msg = importFresh(join(outDir, fileName)) as {
     default: any;
